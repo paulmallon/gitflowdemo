6c1b0d919baa626a8c9c96642704f5f331660bbd
PM <paul.james.mallon@gmail.com>
2019-09-16 14:42:16 +0200

    Logger added!

--- src/main/java/nullterrier/com/demo/DemoApplication.java
+++ src/main/java/nullterrier/com/demo/DemoApplication.java
@@ -1,10 +1,14 @@
 package nullterrier.com.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
